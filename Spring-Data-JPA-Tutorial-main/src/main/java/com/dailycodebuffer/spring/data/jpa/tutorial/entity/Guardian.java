077977e5720e6e274ef3c6601c9725fa3a6ada50
Saurabh Kumar Singh <77205761+saurabhsks@users.noreply.github.com>
2023-06-15 23:32:37 +0530

    Add files via upload

---
+++ Spring-Data-JPA-Tutorial-main/src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/Guardian.java
@@ -0,0 +1,37 @@
+package com.dailycodebuffer.spring.data.jpa.tutorial.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@AttributeOverrides({ //here it is used because these columns are also present in the student table so basically we are using it in the guardian table ,so we are overriding the attributes
+        @AttributeOverride(
+                name = "name",
+                column = @Column(name = "guardian_name")
+        ),
+        @AttributeOverride(
+                name = "email",
+                column = @Column(name = "guardian_email")
+        ),
+        @AttributeOverride(
+                name = "mobile",
+                column = @Column(name = "guardian_mobile")
+        )
+})
+public class Guardian {
+
+    private String name;
+    private String email;
+    private String mobile;
+}
---
+++ Spring-Data-JPA-Tutorial-main/src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/TeacherRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.dailycodebuffer.spring.data.jpa.tutorial.repository;
+
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Course;
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Teacher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TeacherRepositoryTest {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Test
+    public void saveTeacher() {
+
+        Course courseDSA = Course.builder()
+                .title("DSA")
+                .credit(5)
+                .build();
+
+        Course courseJava = Course.builder()
+                .title("Java")
+                .credit(6)
+                .build();
+
+        Teacher teacher =
+                Teacher.builder()
+                        .firstName("Sanjay")
+                        .lastName("Kumar")
+                        //.courses(List.of(courseDSA,courseJava))
+                        .build();
+
+        teacherRepository.save(teacher);
+    }
+}
